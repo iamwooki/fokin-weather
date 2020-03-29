f1d9c15b51a11edf0091f495db11bf7f25854002
iamwooki <hwhong.korea@gmail.com>
2020-03-30 01:58:01 +0900

    update

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
-  );
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+      console.log(coords.altitude, coords.longitude);
+    } catch(error) {
+        Alert.alert("can't find you.","So sad");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  };
+  render(){
+    const { isLoading } =this.state;
+    return isLoading ? <Loading /> : null;
+  };
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- readme.md
+++ readme.md
@@ -13,7 +13,7 @@ npm install -g expo-cli
     - (iOS) 개발자 계정 및 기타 세부 과정 필요
     - native file 제어가능
 
-### zsh: command not found: expo
+### ```zsh: command not found: expo```
 ```
 # NPM global installs, react-native 폴더에서 하기
 export PATH=$PATH:~/.npm-global/bin
@@ -31,3 +31,15 @@ sudo xcode-select -s /Applications/Xcode.app
 ### 명령어
 - Command(Ctrl) + D
     - Debug Remote JS : 크롬 개발자 도구를 이용해 디버깅, 앱 속도 저하 (네트워크 요청, 콘솔 로그 작업 등에만 사용할 것)
+
+### 모바일 앱 만들기
+1. natvie
+2. (hybrid) Webview based on App
+    - (shall)Cordova or PhoneGap
+    - 훌륭한 반응형 웹사이트에 몇가지 네이티브 기능이 필요할 때 (알람/푸쉬) ex) 아마존, 인스타그램 같은
+    - iOS, Android <--- bridge ---> JS script 
+        - 브릿지로 많은 데이터를 보내면 부하가 걸려 성능 저하가 있을 수도, 게임 앱이나 VR/AR엔 적합하지 않음
+
+### Location
+- ref : expo/docs/location
+```expo install expo-location```
